8cb230b0305ed906162b696795b47702d79ad68d
Pinapelz <yukais@pinapelz.com>
2023-10-09 10:26:27 -0700

    feat: add announcement component to landing page - Shows message to users based on whats stored in KV on lanyard

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,33 +1,50 @@
-import VideoGrid from "../components/VideoGrid";
-import Divider from "../components/Divider";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import HeadTags from "../components/HeadTags";
+import Announcement from "../components/Announcement";
+import VideoGrid from "../components/VideoGrid";
 
 function LandingPage() {
   const [storageUsed, setStorageUsed] = useState(0);
   const [numberOfVideos, setNumberOfVideos] = useState(0);
+  const [announcementMessage, setAnnouncementMessage] = useState("");
 
-  fetch('https://archive.pinapelz.moe/api/storage/status')
-  .then(response => response.json())
-  .then(data => {
-    setStorageUsed(data.storage_size);
-    setNumberOfVideos(data.number_of_files);
-  })
-  .catch(error => console.error(error));
+  useEffect(() => {
+    fetch("https://archive.pinapelz.moe/api/storage/status")
+      .then((response) => response.json())
+      .then((data) => {
+        setStorageUsed(data.storage_size);
+        setNumberOfVideos(data.number_of_files);
+      })
+      .catch((error) => console.error(error));
+
+    fetch("https://api.lanyard.rest/v1/users/246787839570739211")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success && data.data.kv && data.data.kv.patchworkAnnouncement) {
+          const message = data.data.kv.patchworkAnnouncement;
+          if (message !== "None") {
+            setAnnouncementMessage(message);
+          }
+        }
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <>
-        <HeadTags 
-        title="Patchwork Archive" 
+      <HeadTags
+        title="Patchwork Archive"
         description="Preserving Cultured Rhythm For the Future"
         image="https://patchwork.moekyun.me/favicon.png"
         url=""
-        />
+      />
+      {announcementMessage ? (
+        <Announcement message={announcementMessage} />
+      ) : null}
       <main className="max-w-screen-xl mx-auto px-4 mt-8">
         <div className="text-center mb-8">
           <h2 className="text-4xl font-bold mb-4 mt-16 text-white">
-            Welcome to the VTuber Music Archives
+            Welcome
           </h2>
           <p className="text-xl text-gray-400">
             We have {numberOfVideos} videos archived taking up {storageUsed}GB of storage space.
@@ -41,13 +58,9 @@ function LandingPage() {
           apiUrl="https://archive.pinapelz.moe/api/discover_videos"
           titleText="Discover"
         />
-        <Divider className="my-4" />
-        <VideoGrid
-          apiUrl="https://archive.pinapelz.moe/api/recently_archived"
-          titleText="Recently Archived"
-        />
       </main>
     </>
   );
 }
+
 export default LandingPage;
