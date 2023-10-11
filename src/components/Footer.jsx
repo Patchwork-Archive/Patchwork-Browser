0ece5a4a1ba6b3f20b78e48466bdc7099d565fad
Pinapelz <yukais@pinapelz.com>
2023-10-11 00:42:44 -0700

    update footer with clickable link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer(){
     return(
         <footer>
-            <h1 className="justify-center text-gray-300 font-bold flex py-1">Made with ⚙️ by Pinapelz</h1>
+            <h1 className="justify-center text-gray-300 font-bold flex py-1">Made with ⚙️ by&nbsp;<a className="hover:underline" rel="noreferrer" target="_blank" href="https://pinapelz.moe">Pinapelz</a>
+            </h1>
             <a href="https://github.com/Patchwork-Archive/" className="justify-center text-gray-300 flex py-2 hover:underline">Source Code</a>
         </footer>
     );
-
 }
 export default Footer;
