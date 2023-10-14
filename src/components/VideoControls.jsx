8de7d68bf003f3b4794512125faa5b0e7b28b353
Pinapelz <yukais@pinapelz.com>
2023-10-14 00:10:03 -0700

    fix: slider scrolling adjustment and volume orientation - Made video slider/scrubber not adjustable via scrolling when focused - Rotated volume slider to be vertical

--- src/components/VideoControls.jsx
+++ src/components/VideoControls.jsx
@@ -1,7 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPause, faExpand, faCompress, faVolumeUp, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
-import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faPause,
+  faExpand,
+  faCompress,
+  faVolumeUp,
+  faVolumeMute,
+} from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 const VideoControls = ({ videoRef, isPlaying, onPlayStateChange }) => {
   const [isFullscreen, setIsFullscreen] = useState(false);
@@ -14,23 +21,29 @@ const VideoControls = ({ videoRef, isPlaying, onPlayStateChange }) => {
     const hrs = Math.floor(seconds / 3600);
     const mins = Math.floor((seconds % 3600) / 60);
     const secs = Math.floor(seconds % 60);
-    if(hrs == 0){
-      return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+    if (hrs == 0) {
+      return `${mins.toString().padStart(2, "0")}:${secs
+        .toString()
+        .padStart(2, "0")}`;
     }
     // Not that I expect to have any music videos over 1 hour, but just in case
-    return `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+    return `${hrs.toString().padStart(2, "0")}:${mins
+      .toString()
+      .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
   };
 
   useEffect(() => {
     const updateProgress = () => {
-      const newProgress = (videoRef.current?.currentTime / videoRef.current?.duration) * 100;
+      const newProgress =
+        (videoRef.current?.currentTime / videoRef.current?.duration) * 100;
       setCurrentTime(videoRef.current?.currentTime || 0);
       setProgress(newProgress || 0);
     };
 
     const currentVideoRef = videoRef.current;
     currentVideoRef?.addEventListener("timeupdate", updateProgress);
-    return () => currentVideoRef?.removeEventListener("timeupdate", updateProgress);
+    return () =>
+      currentVideoRef?.removeEventListener("timeupdate", updateProgress);
   }, [videoRef]);
 
   const handlePlayPause = () => {
@@ -66,52 +79,73 @@ const VideoControls = ({ videoRef, isPlaying, onPlayStateChange }) => {
     }
   };
 
+  const handleWheel = (e) => {
+    e.preventDefault();
+    e.target.blur();
+  };
+
   return (
     <div className="video-controls bg-gray-700 bg-opacity-75 p-1 rounded space-x-4 flex items-center">
-      <button 
+      <button
         onClick={handlePlayPause}
         className="p-2 rounded-full hover:bg-gray-600 transition"
       >
-        <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} className="text-white" size="lg" />
+        <FontAwesomeIcon
+          icon={isPlaying ? faPause : faPlay}
+          className="text-white"
+          size="lg"
+        />
       </button>
       <div className="text-white ml-3">
-          {formatTime(currentTime)} / {formatTime(videoRef.current?.duration || 0)}
-        </div>
+        {formatTime(currentTime)} /{" "}
+        {formatTime(videoRef.current?.duration || 0)}
+      </div>
       <div className="relative flex-grow mx-4 flex items-center">
-        <input 
+        <input
           type="range"
           value={progress}
           onChange={handleProgressChange}
+          onWheel={handleWheel}
           className="w-full cursor-pointer slider-thumb bg-red-500"
           title=""
         />
       </div>
 
-      <button 
+      <button
         onClick={() => setShowVolumeSlider(!showVolumeSlider)}
         className="p-2 rounded-full hover:bg-gray-600 transition relative"
       >
-        <FontAwesomeIcon icon={volume > 0 ? faVolumeUp : faVolumeMute} className="text-white" size="lg" />
+        <FontAwesomeIcon
+          icon={volume > 0 ? faVolumeUp : faVolumeMute}
+          className="text-white"
+          size="lg"
+        />
         {showVolumeSlider && (
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 w-20">
-            <input 
-              type="range" 
-              value={volume} 
+          <div className="absolute mb-7 bottom-8 left-1/2 transform -translate-x-1/2 w-20">
+            <input
+              type="range"
+              value={volume}
               onChange={handleVolumeChange}
-              min="0" max="1" 
-              step="0.01" 
+              min="0"
+              max="1"
+              step="0.01"
               className="w-full cursor-pointer slider-thumb mb-4"
               title=""
+              style={{ transform: "rotate(270deg)" }}
             />
           </div>
         )}
       </button>
 
-      <button 
+      <button
         onClick={toggleFullscreen}
         className="p-2 rounded-full hover:bg-gray-600 transition"
       >
-        <FontAwesomeIcon icon={isFullscreen ? faCompress : faExpand} className="text-white" size="lg" />
+        <FontAwesomeIcon
+          icon={isFullscreen ? faCompress : faExpand}
+          className="text-white"
+          size="lg"
+        />
       </button>
     </div>
   );
@@ -119,11 +153,11 @@ const VideoControls = ({ videoRef, isPlaying, onPlayStateChange }) => {
 
 VideoControls.propTypes = {
   videoRef: PropTypes.oneOfType([
-    PropTypes.func, 
-    PropTypes.shape({ current: PropTypes.instanceOf(Element) })
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
   ]),
   isPlaying: PropTypes.bool.isRequired,
-  onPlayStateChange: PropTypes.func.isRequired
+  onPlayStateChange: PropTypes.func.isRequired,
 };
 
 export default VideoControls;
