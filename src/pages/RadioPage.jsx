6fd77636a95ae5bb91618e6f68fd82ecd9ced021
Pinapelz <yukais@pinapelz.com>
2024-02-24 23:46:22 -0800

    add optional for changing radio audio tag to embedded player

--- src/pages/RadioPage.jsx
+++ src/pages/RadioPage.jsx
@@ -1,74 +1,71 @@
-import { useRef } from "react";
+import { useState } from "react";
 import HeadTags from "../components/HeadTags";
 
 function RadioPage() {
-  const radioUrl = import.meta.env.VITE_RADIO_URL_MP3;
-  const m3uAPIUrl = import.meta.env.VITE_RADIO_M3U;
-  const plsAPIUrl = import.meta.env.VITE_RADIO_PLS;
-  const audioRef = useRef(null);
+    const m3uAPIUrl = import.meta.env.VITE_RADIO_M3U;
+    const plsAPIUrl = import.meta.env.VITE_RADIO_PLS;
+    const radioUrl = import.meta.env.VITE_RADIO_URL_MP3;
+    const embedUrl = import.meta.env.VITE_EMBED_URL;
+    const [useEmbed, setUseEmbed] = useState(!embedUrl);
 
-  const handlePlayPause = () => {
-    if (audioRef.current) {
-      audioRef.current.currentTime = audioRef.current.duration;
-    }
-  };
-
-  return (
-    <>
-      <HeadTags
-        title="Patchwork Archive - Radio"
-        description="Listen to a stream of all music archived on Patchwork"
-        image="https://patchwork.moekyun.me/favicon.png"
-        url="/playlist"
-      />
-      {radioUrl ? (
+    return (
         <>
-          <div className="mt-4 text-white text-center">
-            <h1 className="text-2xl">Patchwork Radio Beta!</h1>
-            <p>
-              Listen to a stream of all music archived on Patchwork <br />
-              Synced up with everyone else listening
-              <br />
-              <br />
-              Currently being tested, expect some downtime and changes
-            </p>
-          </div>
-          <div className="mx-auto max-w-md py-8">
-            <audio
-              ref={audioRef}
-              controls
-              autoPlay
-              className="w-full"
-              src={radioUrl}
-              onPlay={handlePlayPause}
-              onPause={handlePlayPause}
+            <HeadTags
+                title="Patchwork Archive - Radio"
+                description="Listen to a stream of all music archived on Patchwork"
+                image="https://patchwork.moekyun.me/favicon.png"
+                url="/playlist"
             />
-          </div>
-          <div className="mt-4 text-white text-center">
-            {m3uAPIUrl ? (
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
-                onClick={() => (window.location.href = m3uAPIUrl)}
-              >
-                Download M3U
-              </button>
-            ) : null}
-            {plsAPIUrl ? (
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
-                onClick={() => (window.location.href = plsAPIUrl)}
-              >
-                Download PLS
-              </button>
-            ) : null}
-          </div>
+            <div className="mt-4 text-white text-center">
+                <h1 className="text-2xl">Patchwork Radio Beta!</h1>
+                <p>
+                    Listen to a stream of all music archived on Patchwork <br />
+                    Synced up with everyone else listening
+                    <br />
+                    <br />
+                    Currently being tested, expect some downtime and changes
+                </p>
+                {embedUrl && (
+                    <a
+                        className=" hover:underline text-white font-bold py-2 mt-2 px-4 mx-2 rounded cursor-pointer"
+                        onClick={() => setUseEmbed(!useEmbed)}
+                    >
+                        {useEmbed ? "Switch to Audio Player" : "Switch to Embed Player"}
+                    </a>
+                )}
+            </div>
+            <div className="mx-auto max-w-md py-8">
+                {useEmbed ? (
+                    <iframe 
+                        src={embedUrl} 
+                        frameborder="0" 
+                        allowtransparency="true" 
+                        style={{width: "100%", minHeight: "150px", border: "0"}}
+                    />
+
+                ) : (
+                    <audio controls autoPlay className="w-full" src={radioUrl} />
+                )}
+            </div>
+            <div className="mt-4 text-white text-center">
+                {m3uAPIUrl ? (
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+                        onClick={() => (window.location.href = m3uAPIUrl)}
+                    >
+                        Download M3U
+                    </button>
+                ) : null}
+                {plsAPIUrl ? (
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+                        onClick={() => (window.location.href = plsAPIUrl)}
+                    >
+                        Download PLS
+                    </button>
+                ) : null}
+            </div>
         </>
-      ) : (
-        <div className="mt-4 text-2xl text-white text-center">
-          <p>Radio is disabled</p>
-        </div>
-      )}
-    </>
-  );
+    );
 }
 export default RadioPage;
