f2823f288e4ca5ed7a19d0465b88188767667b51
Pinapelz <yukais@pinapelz.com>
2024-02-29 20:02:24 -0800

    add lazy loading to all pages  to reduce js bundle size

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,41 @@
+/* eslint-disable react-refresh/only-export-components */
 import ReactDOM from "react-dom/client";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import VideoPage from "./pages/VideoPage";
+import {lazy, Suspense} from "react";
 import Navbar from "./components/Navbar";
-import SearchResultPage from "./pages/SearchResultPage";
-import ChannelPage from "./pages/ChannelPage";
-import StatusPage from "./pages/StatusPage";
-import PlaylistPage from "./pages/PlaylistPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import RadioPage from "./pages/RadioPage";
-import AboutPage from "./pages/AboutPage";
 import { HelmetProvider } from "react-helmet-async";
 import "./styles/index.css";
+
+const LandingPage = lazy(() => import('./pages/LandingPage'));
+const VideoPage = lazy(() => import('./pages/VideoPage'));
+const SearchResultPage = lazy(() => import('./pages/SearchResultPage'));
+const ChannelPage = lazy(() => import('./pages/ChannelPage'));
+const StatusPage = lazy(() => import('./pages/StatusPage'));
+const PlaylistPage = lazy(() => import('./pages/PlaylistPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const RadioPage = lazy(() => import('./pages/RadioPage'));
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+
 
 const helmetContext = {};
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider context={helmetContext}>
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/watch" element={<VideoPage />} />
-        <Route path="/results" element={<SearchResultPage />} />
-        <Route path="/channel/:channelID" element={<ChannelPage />} />
-        <Route path="/status" element={<StatusPage />} />
-        <Route path="/playlist" element={<PlaylistPage />} />
-        <Route path="/radio" element={<RadioPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      {/* TODO: Make something here for Suspense API*/}
+      <Suspense fallback={<div>Loading...</div>}> 
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/watch" element={<VideoPage />} />
+          <Route path="/results" element={<SearchResultPage />} />
+          <Route path="/channel/:channelID" element={<ChannelPage />} />
+          <Route path="/status" element={<StatusPage />} />
+          <Route path="/playlist" element={<PlaylistPage />} />
+          <Route path="/radio" element={<RadioPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </HelmetProvider>
 );
